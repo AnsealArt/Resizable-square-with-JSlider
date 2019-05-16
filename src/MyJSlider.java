a135bf8df2bfbc7b3ca1262440156a75e1fd9997
Anseal <dzieciol.filip.it@gmail.com>
2019-05-16 21:40:22 +0200

    Show square
    Adding a new Object and setting square and slider in frame

--- src/MyJSlider.java
+++ src/MyJSlider.java
@@ -7,11 +7,17 @@ public class MyJSlider {
         frame.setLayout(new BorderLayout());
         frame.setTitle("Resizable square");
 
+        PaintSquare mySquare = new PaintSquare();
+        mySquare.setPreferredSize(new Dimension(400, 400));
+
         JSlider slider = new JSlider();
         slider.setMajorTickSpacing(20);
         slider.setMinorTickSpacing(10);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
+
+        frame.add(mySquare, BorderLayout.CENTER);
+        frame.add(slider, BorderLayout.SOUTH);
 
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.pack();
