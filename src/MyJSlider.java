f817dcc3f902722d06d6d7922510c6a76d480498
Anseal <dzieciol.filip.it@gmail.com>
2019-05-16 21:24:51 +0200

    Set JSlider
    Creating a new Object JSlider

--- src/MyJSlider.java
+++ src/MyJSlider.java
@@ -6,7 +6,13 @@ public class MyJSlider {
         JFrame frame = new JFrame("");
         frame.setLayout(new BorderLayout());
         frame.setTitle("Resizable square");
-        
+
+        JSlider slider = new JSlider();
+        slider.setMajorTickSpacing(20);
+        slider.setMinorTickSpacing(10);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
