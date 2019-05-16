f973da27b15950513b08cd2159e09e2d1df9c27e
Anseal <dzieciol.filip.it@gmail.com>
2019-05-16 21:12:01 +0200

    Dispose JFrame
    Adding basic JFrame settings

--- src/MyJSlider.java
+++ src/MyJSlider.java
@@ -1,7 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyJSlider {
     MyJSlider() {
         JFrame frame = new JFrame("");
+        frame.setLayout(new BorderLayout());
+        frame.setTitle("Resizable square");
+        
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
